b2ae0055551bba8c4f054fc988d99b26577583d8
stormtechie <techconnectsn@gmail.com>
2016-08-02 14:01:17 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,30 @@
+function myFunction() {
+            var popup = document.getElementById('myPopup');
+            popup.classList.toggle('show');
+        }
+        
+$(document).ready(function(){
+            $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+              if (this.hash !== "") {
+                event.preventDefault();
+                var hash = this.hash;
+                $('html, body').animate({
+                  scrollTop: $(hash).offset().top
+                }, 900, function(){
+                  window.location.hash = hash;
+                });
+                } 
+                });
+            $(window).scroll(function() {
+              $(".slideanim").each(function(){
+                var pos = $(this).offset().top;
+          
+                var winTop = $(window).scrollTop();
+                  if (pos < winTop + 600) {
+                    $(this).addClass("slide");
+                  }
+                });
+                });
+                })
+                
+                
